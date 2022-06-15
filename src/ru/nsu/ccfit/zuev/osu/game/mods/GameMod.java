84df5816e96af51bbbf0c1eff1190e67b9579877
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-06-15 14:47:20 +0300

    Update GameMod.java

--- src/ru/nsu/ccfit/zuev/osu/game/mods/GameMod.java
+++ src/ru/nsu/ccfit/zuev/osu/game/mods/GameMod.java
@@ -5,24 +5,26 @@ public enum GameMod {
      * Temporarily unranked mods (until adjusted):
      * sd, pf, sc, re, fl
      */
-    MOD_NOFAIL("nf", 0.5f),
-    MOD_AUTO("auto", 0, true),
-    MOD_EASY("es", 0.5f),
+    MOD_NOFAIL("nf", 1, true),
+    MOD_AUTO("auto", 1, true),
+    MOD_EASY("es", 1),
     MOD_HARDROCK("hr", 1.06f),
     MOD_HIDDEN("hd", 1.06f),
-    MOD_RELAX("relax", 0.001f, true),
-    MOD_AUTOPILOT("ap", 0.001f, true),
+    MOD_RELAX("relax", 1),
+    MOD_AUTOPILOT("ap", 1),
     MOD_DOUBLETIME("dt", 1.12f),
     MOD_NIGHTCORE("nc", 1.12f),
-    MOD_HALFTIME("ht", 0.3f),
-    MOD_SUDDENDEATH("sd", 1, true),
-    MOD_PERFECT("pf", 1, true),
-    MOD_FLASHLIGHT("fl", 1.12f, true),
+    MOD_HALFTIME("ht", 1),
+    MOD_SUDDENDEATH("sd", 1),
+    MOD_PERFECT("pf", 1),
+    MOD_FLASHLIGHT("fl", 1.12f),
     MOD_PRECISE("pr", 1.06f),
-    MOD_SMALLCIRCLE("sc", 1.06f, true),
-    MOD_REALLYEASY("re", 0.5f, true),
-    MOD_SCOREV2("v2", 1, true),
-    MOD_SPEEDUP("su", 1.06f, true);
+    MOD_SMALLCIRCLE("sc", 1.06f),
+    MOD_REALLYEASY("re", 1),
+    MOD_SCOREV2("v2", 1),
+    MOD_SPEEDUP("su", 1.06f),
+    MOD_SCOREHAX("sh", 69420f, true), //custom mod for getting more score
+    MOD_ALWAYSX("ax", 1, true); //custom mod for getting always ss
 
     public final String shortName;
     public final float scoreMultiplier;
