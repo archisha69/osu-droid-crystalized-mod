694a0f7bd3784b95b03072313f54b9bf709e503d
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-06-16 20:03:27 +0300

    changed d score minimum from 0.5f to 0.3f

--- src/ru/nsu/ccfit/zuev/osu/scoring/StatisticV2.java
+++ src/ru/nsu/ccfit/zuev/osu/scoring/StatisticV2.java
@@ -336,7 +336,7 @@ public class StatisticV2 implements Serializable {
         if ((hit300) / (float) notes > 0.6f) {
             return "C";
         }
-       if ((hit300) / (float) notes > 0.5f){
+       if ((hit300) / (float) notes > 0.3f){
            return "D";
        }
        return "F";
